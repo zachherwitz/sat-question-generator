b6b1b20bc7d45ecdfac579644346891754ab2c3d
Zach Herwitz <zach.herwitz@gmail.com>
2020-07-08 11:31:21 -0400

    dummy data and table updates

--- sat.sql
+++ sat.sql
@@ -1,4 +1,1 @@
-CREATE TABLE questions (id SERIAL PRIMARY KEY, question TEXT, answer1 TEXT, answer2 TEXT, answer3 TEXT, answer4 TEXT, tags TEXT);
-INSERT INTO questions (question, answer1, answer2, answer3, answer4, tags) VALUES ('test question', 'a', 'b', 'c', 'd', 'math');
-INSERT INTO questions (question, answer1, answer2, answer3, answer4, tags) VALUES ('test question2', 'a', 'b', 'c', 'd', 'reading');
-INSERT INTO questions (question, answer1, answer2, answer3, answer4, tags) VALUES ('test question3', 'a', 'b', 'c', 'd', 'history');
+CREATE TABLE questions (id SERIAL PRIMARY KEY, question TEXT, answer1 TEXT, answer2 TEXT, answer3 TEXT, answer4 TEXT, tags TEXT, correctanswer TEXT);
