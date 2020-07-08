4efdd368fe55ce8e2cbfcf50dbf711da8b212c64
Zach Herwitz <zach.herwitz@gmail.com>
2020-07-08 15:55:03 -0400

    all questions view working

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ class AllQuestions extends React.Component {
   render = () => {
     return <div className="home">
       <h1> All Questions Test </h1>
+      <div>
+        {this.props.questions.map((question, index) => {
+          return <li key={index}>{question.question}</li>
+        })}
+      </div>
     </div>
   }
 }
@@ -23,39 +28,57 @@ class Home extends React.Component {
 }
 class Nav extends React.Component {
   render = () => {
-    const {changedisplay} = this.props;
+    const {changedisplay, loadQuestions} = this.props;
     return <nav>
       <ul>
         <li><a onClick={changedisplay} id="home">Home</a></li>
         <li><a onClick={changedisplay} id="new-question">New Question</a></li>
-        <li><a onClick={changedisplay} id="all-questions">View All Questions</a></li>
+        <li><a onClick={loadQuestions} id="all-questions">View All Questions</a></li>
       </ul>
     </nav>
   }
 }
 
-class App extends React.Component {  
+class App extends React.Component {
   state = {
     questions: [],
     display: 'home'
   }
-  
+
+  // READ //
+  getQuestions = () => {
+    axios.get('/sat').then(
+      (response) => {
+        console.log(response.data);
+        this.setState({
+          questions:response.data
+        })
+      }
+    )
+  }
+
   //function that changes page view based on nav link
   changeDisplay = (event) => {
     this.setState({
       display: event.target.getAttribute('id')
     }, () => {
       console.log(this.state.display);
-      
+
     })
   }
+
+  loadQuestions = (event) => {
+    this.changeDisplay(event);
+    this.getQuestions();
+  }
+
   render = () => {
     return <div className="container">
-      <Nav changedisplay={this.changeDisplay}/> 
+      <Nav getQuestions={this.getQuestions} loadQuestions={this.loadQuestions} changedisplay={this.changeDisplay}/>
       {/* ternary statement determines what goes here */}
-      {(this.state.display === 'home') ? 
+      {(this.state.display === 'home') ?
         <Home /> : (this.state.display === 'new-question') ?
-          <NewQuestion /> : <AllQuestions />
+          <NewQuestion /> : <AllQuestions questions={this.state.questions} />
       }
     </div>
   }
