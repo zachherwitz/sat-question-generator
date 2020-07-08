815ae7addda3a465868d562fa3b82cc64c730e85
Zach Herwitz <zach.herwitz@gmail.com>
2020-07-08 12:31:18 -0400

    updated backend

--- controllers/questions.php
+++ controllers/questions.php
@@ -2,6 +2,9 @@
 
 include_once __DIR__ . '/../models/testprep.php';
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 if ($_REQUEST['action'] === 'index') {
   echo json_encode(Questions::all());
 } elseif ($_REQUEST['action'] === 'post') {
--- sat.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE questions (id SERIAL PRIMARY KEY, question TEXT, answer1 TEXT, answer2 TEXT, answer3 TEXT, answer4 TEXT, tags TEXT, correctanswer TEXT);
