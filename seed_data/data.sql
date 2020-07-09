ae4af0916ab7e01659b05883b8cffa895864d51e
Zach Herwitz <zach.herwitz@gmail.com>
2020-07-09 08:30:07 -0400

    data dump

--- seed_data/data.sql
+++ seed_data/data.sql
@@ -2,24 +2,256 @@ INSERT INTO questions
   (question, answer1, answer2, answer3, answer4, tags, correctanswer)
 VALUES
   (
-    'The average number of students per classroom, y, at Central High School can
-be estimated using the equation y = 0.8636x + 27.227, where x represents the
-number of years since 2004 and x ≤ 10. Which of the following statements is
-the best interpretation of the number 0.8636 in the context of this problem?',
+    'The average number of students per classroom, y, at Central High School can be estimated using the equation y = 0.8636x + 27.227, where x represents the number of years since 2004 and x ≤ 10. Which of the following statements is the best interpretation of the number 0.8636 in the context of this problem?',
     'A) The estimated average number of students per classroom in 2004',
     'B) The estimated average number of students per classroom in 2014',
     'C) The estimated yearly decrease in the average number of students per classroom',
     'D) The estimated yearly increase in the average number of students per classroom',
     'math',
     'd');
+
+
 INSERT INTO questions
   (question, answer1, answer2, answer3, answer4, tags, correctanswer)
 VALUES
   (
-    'If 2/a-1 = 4/y, and y != 0 where a!= 1, what is y in terms of a?',
-    'A) y = 2a - 2',
-    'B) y = 2a -4',
-    'C) y = 2a - 1/2',
-    'D) y = 1/2(a) + 1',
+    'On Saturday afternoon, Armand sent m text messages each hour for 5 hours, and Tyrone sent p
+text messages each hour for 4 hours. Which of the following represents the total number of messages
+sent by Armand and Tyrone on Saturday afternoon?',
+    'A) 9mp',
+    'B) 20mp',
+    'C) 5m + 4p',
+    'D) 4m + 5p',
     'math',
     'a');
+
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    'If 2 / (a - 1) = 4 / y where y != 0 and a != 1, what is y in terms of a?',
+    'A) y = 2a - 2',
+    'B) y = 2a - 4',
+    'C) y = 2a - 1/2',
+    'D) y = (1/2)a + 1',
+    'math',
+    'a');
+
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    'If y = x^3 + 2x + 5 and z = x^2 + 7x + 1, what is 2y + z in terms of x?',
+    'A) 3x^3 + 11x + 11',
+    'B) 2x^3 + x^2 + 9x + 6',
+    'C) 2x^3 + x^2 + 11x + 11',
+    'D) 2x^3 + 2x^2 + 18x + 12',
+    'math',
+    'c');
+
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    'If a^(1/2) = x where a > 0 and x > 0, which of the following equations gives a in terms of x?',
+    'A) a = 1 / sqrt(x)',
+    'B) a = 1 / x^2',
+    'C) a = sqrt(x)',
+    'D) a = -x^2',
+    'math',
+    'b');
+
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    'The graph of y equals open parentheses 2 x minus 4 close parentheses open parentheses x minus 4 close parentheses is a parabola in the xy-plane. In which of the following equivalent equations do the x- and y-coordinates of the vertex of the parabola appear as constants or coefficients?',
+    'A) y = 2x^2 - 12x + 16',
+    'B) y = 2x(x - 6) + 16',
+    'C) y = (2(x - 3)^2) + (-2) ',
+    'D) y = (x - 2)(2x - 8)',
+    'math',
+    'c');
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    'Which of the following is equal to (14 - 2i)(7 + 12i) when i = sqrt(-1)?',
+    'A) 74',
+    'B) 122',
+    'C) 74 + 154i',
+    'D) 122 + 154i',
+    'math',
+    'd');
+
+
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    '4x - y = 3y + 7. X + 8y = 4. Based on these equations, what is the value of the product xy?',
+    'A) -(3/2)',
+    'B) 1/4',
+    'C) 1/2',
+    'D) 11/9',
+    'math',
+    'c');
+
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    '(1/x) + (2/x) = 1/5. Anise needs to complete a printing job using both of the printers in her office. One of the printers is twice as fast as the other, and together the printers can complete the job in 5 hours. The equation above represents the situation described. Which of the following describes what the expression 1/x represents in this equation?',
+    'A) The time, in hours, that it takes the slower printer to complete the printing job alone',
+    'B) The portion of the job that the slower printer would complete in one hour',
+    'C) The portion of the job that the faster printer would complete in two hours',
+    'D) The time, in hours, that it takes the slower printer to complete 1 fifth of the printing job',
+    'math',
+    'b');
+
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    'It is given that sin x = a, where x is the radian measure of an angle and pi/2 < x < pi. If sin w = (-a), which of the following could be the value of w?',
+    'A) pi - x',
+    'B) x - pi',
+    'C) 2pi + x',
+    'D) x - 2pi',
+    'math',
+    'b');
+
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    'A subway system is expanded to provide service to a growing suburb. A bike-sharing program is adopted to encourage nonmotorized transportation. [[To alleviate rush hour traffic jams in a congested downtown area, stoplight timing is coordinated]]. -- By changing the bracketed segment, which choice best maintains the sentence pattern already establilshed in the paragraph?',
+    'A) NO CHANGE',
+    'B) Coordinating stoplight timing can help alleviate rush hour traffic jams in a congested downtown area.',
+    'C) Stoplight timing is coordinated to alleviate rush hour traffic jams in a congested downtown area.',
+    'D) In a congested downtown area, stoplight timing is coordinated to alleviate rush hour traffic jams.',
+    'writing',
+    'c');
+
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    'A subway system is expanded to provide service to a growing suburb. A bike-sharing program is adopted to encourage nonmotorized transportation. 1 To alleviate rush hour traffic jams in a congested downtown area, stoplight timing is coordinated. When any one of these changes [[occur, it is]] likely the result of careful analysis conducted by transportation planners. -- By changing the bracketed segment, which choice best maintains the sentence pattern already establilshed in the paragraph?',
+    'A) NO CHANGE',
+    'B) occur, they are',
+    'C) occurs, they are',
+    'D) occurs, it is',
+    'writing',
+    'd');
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    'The work of transportation planners generally includes evaluating current transportation needs, assessing the effectiveness of existing facilities, and improving those facilities or 3 [[they design]] new ones. -- By changing the bracketed segment, which choice best maintains the sentence pattern already establilshed in the paragraph?',
+    'A) NO CHANGE',
+    'B) to design',
+    'C) designing.',
+    'D) design',
+    'writing',
+    'c');
+
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    'The work of transportation planners generally includes evaluating current transportation needs, assessing the effectiveness of existing facilities, and improving those facilities or they design new ones. Most transportation planners work in or near cities, [[but some are employed in rural areas]] -- By changing the bracketed segment, which choice best maintains the sentence pattern already establilshed in the paragraph?',
+    'A) NO CHANGE',
+    'B) where job opportunities are more plentiful.',
+    'C) and the majority are employed by government agencies.',
+    'D) DELETE the underlined portion and end the sentence with a period.',
+    'writing',
+    'a');
+
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    'Traffic to and from that location would increase at the beginning and end of work shifts. The transportation [[planner’s job]], might involve conducting a traffic count to determine the daily number of vehicles traveling on the road to the new factory -- By changing the bracketed segment, which choice best maintains the sentence pattern already establilshed in the paragraph?',
+    'A) NO CHANGE',
+    'B) planner\'s job',
+    'C) planners job,',
+    'D) planners job',
+    'writing',
+    'b');
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    'If analysis of the traffic count indicates that there is more traffic than the [[current road as it is designed at this time]] can efficiently accommodate, the transportation planner might recommend widening the road to add another lane. -- By changing the bracketed segment, which choice best maintains the sentence pattern already establilshed in the paragraph?',
+    'A) NO CHANGE',
+    'B) current design of the road right now',
+    'C) road as it is now currently designed',
+    'D) current design of the road',
+    'writing',
+    'd');
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    'Transportation planners work closely with a number of community stakeholders, such as government officials and other interested organizations and individuals. [[Next]], representatives from the local public health department might provide input in designing a network of trails and sidewalks to encourage people to walk more -- By changing the bracketed segment, which choice best maintains the sentence pattern already establilshed in the paragraph?',
+    'A) NO CHANGE',
+    'B) For instance,',
+    'C) Furthermore,',
+    'D) Similarly,',
+    'writing',
+    'b');
+
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    'The nature of impeachment: a narrowly channeled exception to the separation of powers maxim. The Federal Convention of 1787 said that. It limited impeachment to high crimes and misdemeanors, and discounted and opposed the term “maladministration.” -- As used in the opening line, "channeled" most nearly means?',
+    'A) worn',
+    'B) sent,',
+    'C) constrained,',
+    'D) siphoned,',
+    'reading',
+    'c');
+
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    '“No one need be afraid that officers who commit oppression will pass with immunity.” “Prosecutions of impeachments will seldom fail to agitate the passions of the whole community,” said Hamilton in the Federalist Papers, number 65. “We divide into parties more or less friendly or inimical to the accused.”* I do not mean political parties in that sense. -- What is the most likely reason the speaker draws a distinction between two types of parties?',
+    'A) To counter the suggestion that impeachment is or should be about partisan politics',
+    'B) To disagree with Hamilton’s claim that impeachment proceedings excite passions,',
+    'C) To contend that Hamilton was too timid in his support for the concept of impeachment,',
+    'D) To argue that impeachment cases are decided more on the basis of politics than on justice,',
+    'reading',
+    'a');
+
+
+INSERT INTO questions
+  (question, answer1, answer2, answer3, answer4, tags, correctanswer)
+VALUES
+  (
+    'In 1894, British psychologist C. Lloyd Morgan published what’s called Morgan’s canon, the principle that suggestions of humanlike mental processes behind an animal’s behavior should be rejected if a simpler explanation will do. Still, people seem to maintain certain expectations, especially when it comes to birds and mammals. “We somehow want to prove they are as ‘smart’ as people,” zoologist Sara Shettleworth says. We want a bird that masters a vexing problem to be employing human-style insight. -- The main purpose of the first paragraph is to...',
+    'A) offer historical background in order to question the uniqueness of two researchers’ findings.',
+    'B) offer interpretive context in order to frame the discussion of an experiment and its results.',
+    'C) introduce a scientific principle in order to show how an experiment’s outcomes validated that principle.',
+    'D) present seemingly contradictory stances in order to show how they can be reconciled empirically.',
+    'reading',
+    'b');
